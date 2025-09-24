457b9cd8019b15b6b35ed965309709c59e3fa650
toufic.f <t.fakhry@ob-soft.com>
2025-09-24 19:00:33 +0300

    feat: Add new classes to model package
    This commit introduces several new classes to the `com.usj.onboardingapp.model` package.
    
    New classes:
    - `AudioBook`
    - `Book`
    - `Library`
    - `LoanStatus`
    - `MediaItem`
    - `Movie`
    - `MovieGenre`
    - `Playable`
    - `Review`

--- app/src/main/java/com/usj/onboardingapp/model/AudioBook.kt
+++ app/src/main/java/com/usj/onboardingapp/model/AudioBook.kt
@@ -1,6 +1,16 @@
 package com.usj.onboardingapp.model
 
-class AudioBook(title: String, year: Int, author: String, pages: Int = 0 , val narrator: String, override val duration: Int, isAvailable: Boolean): Book(title, year, author, pages, isAvailable), Playable {
+class AudioBook(
+    title: String,
+    year: Int,
+    author: String,
+    loanStatus: LoanStatus,
+    pages: Int = 0,
+    val narrator: String,
+    override val duration: Int,
+    isAvailable: Boolean
+) :
+    Book(title, year, author, loanStatus, pages, isAvailable), Playable {
 
     override fun play() {
         println("Playing $title")
